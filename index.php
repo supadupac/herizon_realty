27929cf803167da77506b2de51d86ae40dbc2020
Christopher Lee <chrislee@umn.edu>
2019-01-09 10:18:25 -0600

    fixed image replace

--- index.php
+++ index.php
@@ -42,9 +42,9 @@
 
     <div class="container-fluid light-bg">
       <div class="container">
-        <div class="row agents">
+        <div class="row agents"> 
           <div class="col-xl-5">
-            <img src="http://herizon.christophertoulee.com//wp-content/uploads/2018/09/herizon_agents.jpg" class="img-fluid">
+            <img src="http://herizon.christophertoulee.com/wp-content/uploads/herizon_agents.jpg" class="img-fluid">
           </div>
           <div class="col-xl-7 text-center">
             <h1>What our agents can do for you</h1>
