202da15bf27a8e41057417cad24e508ff775c6db
Christopher Lee <chrislee@umn.edu>
2019-01-10 13:25:53 -0600

    added edit to functions

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@
     wp_enqueue_style('font_awesome_styles', 'https://use.fontawesome.com/releases/v5.2.0/css/all.css');
     wp_enqueue_script('main_javascript', get_theme_file_uri('/js/script.js'), array('jquery'), NULL, '0.1.0', true);
     wp_enqueue_script('main_javascript', get_theme_file_uri('/contact/contact.js'), array('jquery'), NULL, '0.1.0', true);
+    wp_enqueue_script('main_javascript', get_theme_file_uri('/js/contact.js'), array('jquery'), NULL, '0.1.0', true);
   }
 
   add_action('wp_enqueue_scripts', 'herizon_files');
