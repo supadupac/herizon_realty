9b94c355004a9fb4154ba628cd0441ab1a45bcfb
Christopher Lee <chrislee@umn.edu>
2019-03-23 15:28:32 -0500

    removed red div and added feature div

--- front-page.php
+++ front-page.php
@@ -19,10 +19,8 @@
 </div>
 
 <div class="container">
-    <div class="featured">
-        <div class="row red">
-            <?php echo do_shortcode("[optima_express_featured displayType='grid' resultsPerPage='4' header='false' includeMap='false' status='active']"); ?>
-        </div>
+    <div class="row featured">
+        <?php echo do_shortcode("[optima_express_featured displayType='grid' resultsPerPage='4' header='false' includeMap='false' status='active']"); ?>
     </div>
 </div>
 
