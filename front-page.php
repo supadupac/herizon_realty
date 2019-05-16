cef39b3763ed0910976237630fcb376c85047de8
Christopher Lee <chrislee@umn.edu>
2019-05-15 20:42:03 -0500

    changing col to 8

--- front-page.php
+++ front-page.php
@@ -25,7 +25,7 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-12">
+        <div class="col-lg-8">
             <?php echo do_shortcode("[optima_express_featured sortBy='cn' displayType='grid' resultsPerPage='4' header='false' includeMap='false' status='active']"); ?>
         </div>
     </div>
